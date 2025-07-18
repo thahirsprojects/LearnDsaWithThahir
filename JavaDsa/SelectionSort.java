74b9085c1c50f5610b8b40984770e88259697a0d
thahirdesigns <thahirdesanddev@gmail.com>
2025-07-18 18:35:09 +0530

    Create SelectionSort.java

---
+++ JavaDsa/SelectionSort.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class SelectionSort {
+
+    public int[] selectionSort(int[] array){
+        for(int i = 0; i < array.length-1; i++){
+            int minIndex=i;
+            for(int j = i+1;j < array.length;j++){
+                if(array[j] < array[minIndex]){
+                    minIndex = j;
+                }
+            }
+            swap(array,minIndex,i);
+        }
+        return array;
+    }
+
+    public void swap(int[] array,int a,int b){
+        int temp = array[a];
+        array[a] = array[b];
+        array[b] = temp;
+    }
+
+    public static void main(String[] args){
+
+        SelectionSort eg = new SelectionSort();
+        int[] arr = {23,-34,0,43,234};
+
+        System.out.println(Arrays.toString(eg.selectionSort(arr)));
+    }
+}
